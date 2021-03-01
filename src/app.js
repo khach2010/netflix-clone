60bf95de462219d12eda87c017eadff5e96f165b
tu le <tule@tus-MacBook-Pro.local>
2021-03-01 07:01:27 +0100

    IsUserRedirect - ProtectRoute

--- src/app.js
+++ src/app.js
@@ -1,23 +1,41 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
 import * as ROUTES from './constants/routes'
 import { Home, Browse, SignIn, SignUp } from './pages'
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes'
 
 function App() {
+  const user = null
   return (
     <Router>
-      <Route exact path="/browse">
-        <Browse />
-      </Route>
-      <Route exact path="/signin">
-        <SignIn />
-      </Route>
-      <Route exact path="/signup">
-        <SignUp />
-      </Route>
-      <Route exact path={ROUTES.HOME}>
-        <Home />
-      </Route>
+      <Switch>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_IN}
+        >
+          <SignIn />
+        </IsUserRedirect>
+
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_UP}
+        >
+          <SignUp />
+        </IsUserRedirect>
+
+        <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+          <Browse />
+        </ProtectedRoute>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.HOME}
+        >
+          <Home />
+        </IsUserRedirect>
+      </Switch>
     </Router>
   )
 }
