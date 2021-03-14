7e78c49a765151d2fc64bd57d2a2d3d090aaf01f
tu le <khach2010@gmail.com>
2021-03-14 06:52:17 +0100

    search functionality

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,4 +1,5 @@
 import React, { useContext, useState, useEffect } from 'react'
+import Fuse from 'fuse.js'
 import { Loading, Header, Card, Player } from '../components'
 import { SelectionProfileContainer } from './profiles'
 import { FirebaseContext } from '../context/Firebase'
@@ -25,6 +26,19 @@ export function BrowseContainer({ slides }) {
   useEffect(() => {
     setSlideRows(slides[category])
   }, [slides, category])
+
+  useEffect(() => {
+    const fuse = new Fuse(slideRows, {
+      keys: ['data.description', 'data.title', 'data.genre'],
+    })
+    const results = fuse.search(searchTerm).map(({ item }) => item)
+
+    if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+      setSlideRows(results)
+    } else {
+      setSlideRows(slides[category])
+    }
+  }, [searchTerm])
 
   return profile.displayName ? (
     <>
